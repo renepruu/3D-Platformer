5b2d0f115993b65d2a4609e2ef6aa7cf6576a6fd
exoexoexo3 <morphedtononexistent@gmail.com>
2025-12-05 23:41:15 +0200

    Reapply "soundtrack added + movement (has to be fixed)"
    This reverts commit dacb3bd341c6d7d19e7f499f1fff0d5bc8318392.

---
+++ Assets/Audio/FootSteps.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class FootstepPlayer : MonoBehaviour
+{
+    public AudioSource Source;
+    public AudioClip[] footstepSounds;
+
+    public void PlayFootstep()
+    {
+        if (footstepSounds.Length == 0) return;
+
+        int randomIndex = Random.Range(0, footstepSounds.Length);
+        Source.PlayOneShot(footstepSounds[randomIndex]);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayFootstep();
+        }
+    }
+}
